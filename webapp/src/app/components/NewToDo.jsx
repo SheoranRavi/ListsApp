e6ed092adb227a5ff958add1ac1b8b52c7043790
Ravi Sheoran <ravisheoranr@gmail.com>
2023-08-29 23:07:57 +0530

    minor style changes

--- webapp/src/app/components/NewToDo.jsx
+++ webapp/src/app/components/NewToDo.jsx
@@ -17,16 +17,16 @@ export default function NewToDo(props){
     }
 
     return(
-        <form onSubmit={handleSubmit} className="flex justify-center bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form onSubmit={handleSubmit} className="flex justify-center bg-white shadow-md rounded-lg pt-2 pb-2 mb-4">
             <input 
-                className="border border-solid"
+                className="block border border-slate-300 rounded-md drop-shadow-lg py-2 pl-2 pr-3 focus:outline-none focus:border-sky-500 focus:ring-1 sm:text-sm focus:ring-sky-500"
                 type="text" 
                 name="toDoItem"
                 placeholder="Enter To Do Item"
                 required={true}
                 pattern="^.{3,500}$"
             />
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">Add</button>
         </form>
     )
 }

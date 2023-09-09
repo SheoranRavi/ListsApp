90f25163b4564e74dad98872d1b31724b366be7f
Ravi Sheoran <ravisheoranr@gmail.com>
2023-09-09 10:29:07 +0530

    hide scrollbar for desktop

--- webapp/src/app/components/List.jsx
+++ webapp/src/app/components/List.jsx
@@ -6,7 +6,7 @@ export default function List(props){
     const updateItems = props.updateItems;
     console.log("re-rendering List");
     return (
-        <div className="block h-[62vh] sm:h-[66vh] overflow-y-scroll shadow-md shadow-gray-600 
+        <div className="block h-[62vh] sm:h-[66vh] overflow-y-scroll no-scrollbar shadow-md shadow-gray-600 
         mx-2 p-1 rounded-md bg-[#a0d2eb]">
             <ul className='flex flex-col-reverse items-center w-full'>
                 {toDos.map((item) => {
